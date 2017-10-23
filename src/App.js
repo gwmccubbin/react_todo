359975346fae0264cc2f8cf1ba7e573f63516178
Gregory McCubbin <gwmccubbin@gmail.com>
2017-10-23 17:30:28 -0500

    Adds a Todo Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, world!</h1>
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.refs.todo.value);
+  }
+
+  render() {
+    return(
+      <form onSubmit={(e) => this.handleSubmit(e)} >
+        <input type="text" ref="todo" placeholder="Add todo..." />
+        <input type="submit" hidden />
+      </form>
+    )
+  }
+}
+
+export default TodoForm;
