3915c59b9edac5b107a7206540efee10ded14087
Gregory McCubbin <gwmccubbin@gmail.com>
2017-10-25 16:56:15 -0500

    Adds store

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/index';
+
+// import todos from './data/todos';
+
+const defaultState = {
+  // todos
+}
+
+const store = createStore(rootReducer, defaultState);
+
+export default store;
