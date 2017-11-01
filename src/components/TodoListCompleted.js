3acc73c38b28710f8aae0ac2ebea49565ac5184f
Gregory McCubbin <gwmccubbin@gmail.com>
2017-11-01 16:18:30 -0500

    Compares using `===`

--- src/components/TodoListCompleted.js
+++ src/components/TodoListCompleted.js
@@ -6,7 +6,7 @@ class TodoListCompleted extends Component {
     return (
       <ul className="list-unstyled">
         {/* TODO: Clean me up */}
-        {this.props.todos.filter((todo) => todo.completed == true ).map((todo, i) => <TodoCompleted {...this.props} key={this.props.todos.indexOf(todo)} i={this.props.todos.indexOf(todo)} todo={todo} />)}
+        {this.props.todos.filter((todo) => todo.completed === true ).map((todo, i) => <TodoCompleted {...this.props} key={this.props.todos.indexOf(todo)} i={this.props.todos.indexOf(todo)} todo={todo} />)}
       </ul>
     );
   }
